55f02d8e00d83ba5ca1feaf35c523c1c6db5ed64
Waldek <waldek.pieniak@gmail.com>
2019-04-30 23:09:01 +0200

    Initial commit

---
+++ commponents/LoginForm.js
@@ -0,0 +1,29 @@
+import { loginUser } from "../lib/auth";
+
+export class LoginForm extends React.Component {
+    state = {
+        email: 'Sincere@april.biz',
+        password: 'hildegard.org',
+    }
+
+    handleChange = ({target: {name, value}}) => this.setState({
+        [name]: value
+    })
+
+    handleSubmit = event => {
+        const { email, password } = this.state;
+        event.preventDefault()
+        loginUser(email, password);
+    }
+    render() {
+        const { email, password } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div><input type="email" name="email" onChange={this.handleChange} placeholder="email" value={email}/></div>
+                <div><input type="password" name="password" onChange={this.handleChange} placeholder="password" value={password}/></div>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
---
+++ pages/login.js
@@ -0,0 +1,5 @@
+import { LoginForm } from '../commponents/LoginForm';
+
+export default () => (
+    <LoginForm />
+)
